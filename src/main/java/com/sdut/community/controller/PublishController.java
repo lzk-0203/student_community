495719cc92579bd7b21039ee56c86b6dcab03c46
lzk-0203 <2469916640@qq.com>
2021-11-01 21:56:24 +0800

    add question, to be continue

--- src/main/java/com/sdut/community/controller/PublishController.java
+++ src/main/java/com/sdut/community/controller/PublishController.java
@@ -1,15 +1,58 @@
 package com.sdut.community.controller;
 
+import com.sdut.community.mapper.QuestionMapper;
+import com.sdut.community.mapper.UserMapper;
+import com.sdut.community.model.Question;
+import com.sdut.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author 24699
  */
 @Controller
 public class PublishController {
+
+    @Autowired
+    private QuestionMapper questionMapper;
+
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/publish")
     public String publish() {
         return "publish";
+    }
+
+    @PostMapping("/publish")
+    public String doPublish(
+            @RequestParam("pro_title") String pro_title,
+            @RequestParam("description") String description,
+            @RequestParam("tags") String tags,
+            HttpServletRequest request,
+            Model model) {
+        Question question = new Question();
+        question.setTitle(pro_title);
+        question.setDescription(description);
+        question.setTags(tags);
+
+        User user = (User) request.getSession().getAttribute("user");
+        if (user == null) {
+            model.addAttribute("error", "用户未登录");
+            return "publish";
+        }
+        question.setCreator(user.getId());
+        question.setGmtCreate(System.currentTimeMillis());
+        question.setGmtCreate(question.getGmtCreate());
+
+        questionMapper.create(question);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/sdut/community/mapper/QuestionMapper.java
@@ -0,0 +1,20 @@
+package com.sdut.community.mapper;
+
+import com.sdut.community.model.Question;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author 24699
+ */
+@Mapper
+public interface QuestionMapper {
+    /**
+     * 添加问题
+     * @param question
+     * @return
+     */
+    @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tags) " +
+            "values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tags}")
+    int create(Question question);
+}
